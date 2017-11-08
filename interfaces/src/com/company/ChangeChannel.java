e172ffbf50ba66505de96896543ea734f7831f24
kjagla <kjagla@sointeractive.pl>
2017-11-08 11:45:57 +0100

    interfaces

---
+++ interfaces/src/com/company/ChangeChannel.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by karol on 08.11.17.
+ */
+public interface ChangeChannel  {
+
+   public final static int size = 5;
+    public void changeChannel();
+
+}
---
+++ interfaces/src/com/company/Home.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by karol on 08.11.17.
+ */
+public class Home {
+
+
+}
