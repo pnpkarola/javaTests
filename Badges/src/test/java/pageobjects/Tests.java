beccba7ad954d1febee58b6dd724c8927b9860d1
Your Name <you@example.com>
2017-12-23 21:49:00 +0100

    "added 5 sec to implicite wait"

--- Badges/src/test/java/pageobjects/Tests.java
+++ Badges/src/test/java/pageobjects/Tests.java
@@ -20,7 +20,7 @@ public class Tests {
     @BeforeTest
     public void setup() {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(1123, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(112, TimeUnit.SECONDS);
         driver.get(URL);
     }
 
