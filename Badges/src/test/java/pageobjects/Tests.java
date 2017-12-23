8e1cbbc8c891d455eaae0032b66b636394db69a2
Your Name <you@example.com>
2017-12-23 22:03:26 +0100

    "added 10"

--- Badges/src/test/java/pageobjects/Tests.java
+++ Badges/src/test/java/pageobjects/Tests.java
@@ -20,7 +20,7 @@ public class Tests {
     @BeforeTest
     public void setup() {
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(URL);
     }
 
